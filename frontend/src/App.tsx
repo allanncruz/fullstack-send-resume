18c2e8f9ef84684714140820a376456aa8921b21
Allan <contato@allancruz.com.br>
2024-07-19 11:50:44 -0300

    feat: component resume form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import ResumeForm from "./components/ResumeForm";
 
 function App() {
   return (
     <div className="App">
-      Resume
+       <h1>Formulário de Envio de Currículos</h1>
+       <ResumeForm />
     </div>
   );
 }
