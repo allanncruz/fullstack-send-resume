9622c5db25bc1614fb181be494ffc5c7a2c36906
Allan <contato@allancruz.com.br>
2024-07-22 09:05:11 -0300

    feat: config env and conection db

--- backend/public/index.php
+++ backend/public/index.php
@@ -2,16 +2,23 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
+use Doctrine\DBAL\Connection;
+
+$container = new Container();
+
+$container->set(Connection::class, function () {
+    return require __DIR__ . '/../src/config.php';
+});
+
+AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
-// Adicione o middleware de roteamento
 $app->addRoutingMiddleware();
 
-// Defina o roteador
-require __DIR__ . '/../src/routes.php';
-
-// Habilite a exibição de erros (apenas para desenvolvimento)
 $app->addErrorMiddleware(true, true, true);
+
+require __DIR__ . '/../src/routes.php';
 
 $app->run();
--- backend/src/Controllers/ResumeController.php
+++ backend/src/Controllers/ResumeController.php
@@ -3,16 +3,32 @@ namespace App\Controllers;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Doctrine\DBAL\Connection;
 
 class ResumeController
 {
+    protected $db;
+
+    public function __construct(Connection $db)
+    {
+        $this->db = $db;
+    }
+
     public function submitResume(Request $request, Response $response, array $args): Response
     {
         $data = $request->getParsedBody();
         $files = $request->getUploadedFiles();
 
-        // Processar e validar dados aqui
-        // Salvar dados no banco de dados, enviar emails, etc.
+        $this->db->insert('resumes', [
+            'name'             => $data['name'],
+            'email'            => $data['email'],
+            'phone'            => $data['phone'],
+            'desired_position' => $data['desiredPosition'],
+            'education'        => $data['education'],
+            'comments'         => $data['comments'],
+            'resume_file'      => $files['resumeFile']->getClientFilename(),
+            'submitted_at'     => date('Y-m-d H:i:s'),
+        ]);
 
         $response->getBody()->write(json_encode(['message' => 'Resume submitted successfully']));
         return $response->withHeader('Content-Type', 'application/json');
